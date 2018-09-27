55949217b08b575aca680174e3246a788b10fd48
Kentaro Sakae <musamusa0607@gmail.com>
2018-09-27 23:06:06 +0900

    React設定

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from './store';
+
+import App from './components/App';
+
+ReactDom.render(
+  <App />,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const publicDir = path.join(__dirname, '/public');
+
+module.exports = (env, argv) => {
+  const IS_DEVELOPMENT = argv.mode === 'development';
+
+  return ({
+    entry: [
+      './src/index.jsx',
+    ],
+    output: {
+      path: publicDir,
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.js$/, /\.jsx$/],
+          use: [{
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { modules: false }],
+                '@babel/preset-react',
+              ],
+            },
+          }],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+      historyApiFallback: true,
+      contentBase: publicDir,
+    },
+    devtool: IS_DEVELOPMENT ? 'inline-source-map' : '',
+    // console.log()の削除
+    optimization: {
+      minimizer: IS_DEVELOPMENT
+        ? []
+        : [
+          new UglifyJSPlugin({
+            uglifyOptions: {
+              compress: {
+                drop_console: true,
+              },
+            },
+          }),
+        ],
+    },
+  });
+};
